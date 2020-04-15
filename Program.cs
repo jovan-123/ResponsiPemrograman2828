ad782a457de6427c8a9972efea252bee5509db1b
jovan-123 <61934098+jovan-123@users.noreply.github.com>
2020-04-15 12:14:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2828
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan("Mohamad", 192617, 400000);
+            Karyawan karyawan2 = new Karyawan("Jovan", 192618, 300000);
+            Karyawan karyawan3 = new Karyawan("Nikola", 192619, 250000);
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.lihatKaryawan();
+            Console.WriteLine();
+            karyawan2.lihatKaryawan();
+            Console.WriteLine();
+            karyawan3.lihatKaryawan();
+
+            Console.WriteLine("\n\n Asikkk kenaikan gaji cuy");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.NaikGaji();
+            Console.WriteLine();
+            karyawan2.NaikGaji();
+            Console.WriteLine();
+            karyawan3.NaikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
